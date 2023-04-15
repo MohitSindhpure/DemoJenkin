f5d46ff32997851913a6a1e93bfc3d74cab9fe28
MohitSindhpure <mohitsindhpure1998@gmail.com>
2023-04-15 23:20:05 +0530

    first commit

---
+++ src/main/java/com/jenkin/DemoJenkinApplication.java
@@ -0,0 +1,15 @@
+package com.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinApplication.class, args);
+
+		System.out.println("Run Demo Jenkin Project.");
+	}
+
+}
